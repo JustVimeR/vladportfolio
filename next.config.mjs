d570a7b969704f62f86f543d47857ead57bd25ae
Vlad <dog792804@gmail.com>
2025-03-15 22:45:24 +0200

    git pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 // next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
-  
+	output: "export",
+	basePath: "/vladportfolio",
+	images: { unoptimized: true },
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/,
+			use: ["@svgr/webpack"],
+		});
+
+		return config;
+	},
+};
+
+export default nextConfig;
